87dfcea6afd5113a00c4cbfe9a2975ea6a469afc
blair-developer <blairdeveloper17@gmail.com>
2024-05-25 13:32:04 +0300

    first commit

---
+++ enquiry.php
@@ -0,0 +1,297 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>RMS | Rental Management System</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href="css/enq.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+<!-- Custom Theme files -->
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!--animate-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="my_js/wow.min.js"></script>
+	<script>
+		 new WOW().init();
+	</script>
+<!--//end-animate-->
+</head>
+<body>
+<?php include('includes/header.php');?>
+<div class="banner">
+	<div class="container">
+		<h1 class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;"> RMS - Rental Management System</h1>
+	</div>
+</div>
+
+
+<!--- rupes ---->
+<div class="container">
+	<div class="rupes">
+		<div class="col-md-4 rupes-left wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+			<div class="rup-left">
+				<a href="homepage.php"><i class="fa fa-code-fork"></i></a>
+			</div>
+			<div class="rup-rgt">
+				<h3>UP TO USD. 50 OFF</h3>
+				<h4><a href="offers.html">COMPLY SMART</a></h4>
+				
+			</div>
+				<div class="clearfix"></div>
+		</div>
+		<div class="col-md-4 rupes-left wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+			<div class="rup-left">
+				<a href="homepage.php"><i class="fa fa-h-square"></i></a>
+			</div>
+			<div class="rup-rgt">
+				<h3>UP TO 70% OFF</h3>
+				<h4><a href="homepage.php">HOUSES ACROSS</a></h4>
+				
+			</div>
+				<div class="clearfix"></div>
+		</div>
+		<div class="col-md-4 rupes-left wow fadeInDown animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: fadeInDown;">
+			<div class="rup-left">
+				<a href="homepage.php"><i class="fa fa-mobile"></i></a>
+			</div>
+			<div class="rup-rgt">
+				<h3>FLAT USD. 50 OFF</h3>
+				<h4><a href="homepage.php">US APP OFFER</a></h4>
+			
+			</div>
+				<div class="clearfix"></div>
+		</div>
+	
+	</div>
+</div>
+<!--- /rupes ---->
+
+<div class="plans-section" id="rooms">
+				 <div class="container">
+				 <h3 class="title-w3-agileits title-black-wthree">Rooms And Rates</h3>
+						<div class="priceing-table-main">
+				 <div class="col-md-3 price-grid">
+					<div class="price-block agile">
+						<div class="price-gd-top">
+						<img src="images/g3.jpg" alt=" " class="img-responsive" />
+							<h4>One-bed Room</h4>
+						</div>
+						<div class="price-gd-bottom">
+							   <div class="price-list">
+									<ul>
+											<li><i class="fa fa-star" aria-hidden="true"></i></li>
+											<li><i class="fa fa-star" aria-hidden="true"></i></li>
+											<li><i class="fa fa-star" aria-hidden="true"></i></li>
+											<li><i class="fa fa-star" aria-hidden="true"></i></li>
+											<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
+									
+								     </ul>
+							</div><br><br>
+							<div class="price-selet">	
+								<h3><span>Ksh</span>10,000</h3>						
+								<a href="admin/reservation.php" >Book Now</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 price-grid ">
+					<div class="price-block agile">
+						<div class="price-gd-top">
+						<img src="images/g4.jpg" alt=" " class="img-responsive" />
+							<h4>Single Room</h4>
+						</div>
+						<div class="price-gd-bottom">
+							<div class="price-list">
+									<ul>
+									<li><i class="fa fa-star" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
+								</ul>
+							</div><br><br>
+							<div class="price-selet">
+								<h3><span>Ksh</span>5,000</h3>
+								<a href="admin/reservation.php" >Book Now</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 price-grid lost">
+					<div class="price-block agile">
+						<div class="price-gd-top">
+						<img src="images/house.png" alt=" " class="img-responsive" />
+							<h4>Bed-sitter</h4>
+						</div>
+						<div class="price-gd-bottom">
+							<div class="price-list">
+								<ul>
+									<li><i class="fa fa-star" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
+								</ul>
+							</div><br><br><br><br>
+							<div class="price-selet">
+								<h3><span>Ksh</span>8,000</h3>
+								<a href="admin/reservation.php" >Book Now</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 price-grid wthree lost">
+					<div class="price-block agile">
+						<div class="price-gd-top ">
+							<img src="images/about.jpg" alt=" " class="img-responsive" />
+							<h4>Single family Room</h4>
+						</div>
+						<div class="price-gd-bottom">
+							<div class="price-list">
+								<ul>
+									<li><i class="fa fa-star" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
+									<li><i class="fa fa-star-o" aria-hidden="true"></i></li>
+								</ul>
+							</div><br><br><br><br><br><br>
+							<div class="price-selet">
+								<h3><span>Ksh</span> 15,000</h3>
+								<a href="admin/reservation.php" >Book Now</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+
+
+<!---holiday---->
+
+
+
+
+<!--- routes ---->
+<div class="routes">
+	<div class="container">
+		<div class="col-md-4 routes-left wow fadeInRight animated" data-wow-delay=".5s">
+			<div class="rou-left">
+				<a href="#"><i class="glyphicon glyphicon-list-alt"></i></a>
+			</div>
+			<div class="rou-rgt wow fadeInDown animated" data-wow-delay=".5s">
+				<h3>80000</h3>
+				<p>Enquiries</p>
+			</div>
+				<div class="clearfix"></div>
+		</div>
+		<div class="col-md-4 routes-left">
+			<div class="rou-left">
+				<a href="#"><i class="fa fa-user"></i></a>
+			</div>
+			<div class="rou-rgt">
+				<h3>1900</h3>
+				<p>Regestered users</p>
+			</div>
+				<div class="clearfix"></div>
+		</div>
+		<div class="col-md-4 routes-left wow fadeInRight animated" data-wow-delay=".5s">
+			<div class="rou-left">
+				<a href="#"><i class="fa fa-ticket"></i></a>
+			</div>
+			<div class="rou-rgt">
+				<h3>7,00,00,000+</h3>
+				<p>Booking</p>
+			</div>
+				<div class="clearfix"></div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
+
+<section class="contact-w3ls" id="contact">
+	<div class="container">
+		<div class="col-lg-6 col-md-6 col-sm-6 contact-w3-agile2" data-aos="flip-left">
+			<div class="contact-agileits">
+				<h4>Contact Us</h4>
+				<p class="contact-agile2">Sign Up For communication</p>
+				<form  method="post" name="sentMessage" id="contactForm" >
+					<div class="control-group form-group">
+                        
+                            <label class="contact-p1">Full Name:</label>
+                            <input type="text" class="form-control" name="name" id="name" required >
+                            <p class="help-block"></p>
+                       
+                    </div>	
+                    <div class="control-group form-group">
+                        
+                            <label class="contact-p1">Phone Number:</label>
+                            <input type="tel" class="form-control" name="phone" id="phone" required >
+							<p class="help-block"></p>
+						
+                    </div>
+                    <div class="control-group form-group">
+                        
+                            <label class="contact-p1">Email Address:</label>
+                            <input type="email" class="form-control" name="email" id="email" required >
+							<p class="help-block"></p>
+						
+                    </div>
+                    
+                    
+                    <input type="submit" name="sub" value="Send Now" class="btn btn-primary">	
+				</form>
+				<?php
+				if(isset($_POST['sub']))
+				{
+					$name =$_POST['name'];
+					$phone = $_POST['phone'];
+					$email = $_POST['email'];
+					$approval = "Not Allowed";
+					$sql = "INSERT INTO `contact`(`fullname`, `phoneno`, `email`,`cdate`,`approval`) VALUES ('$name','$phone','$email',now(),'$approval')" ;
+					
+					
+					if(mysqli_query($conn,$sql))
+					echo"thank you for contacting us";
+					
+				}
+				?>
+			</div>
+		</div>
+		<div class="col-lg-6 col-md-6 col-sm-6 contact-w3-agile1" data-aos="flip-right">
+			<h4>Connect With Us</h4>
+			<p class="contact-agile1"><strong>Phone :</strong>+254-752335461</p>
+			<p class="contact-agile1"><strong>Email :</strong> <a href="mailto:name@example.com">juvenile@gmail.com</a></p>
+			<p class="contact-agile1"><strong>Address :</strong> Thika Road, Juja, macheo</p>
+																
+			<div class="social-bnr-agileits footer-icons-agileinfo">
+				<ul class="social-icons3">
+								<li><a href="#" class="fa fa-facebook icon-border facebook"> </a></li>
+								<li><a href="#" class="fa fa-twitter icon-border twitter"> </a></li>
+								<li><a href="#" class="fa fa-google-plus icon-border googleplus"> </a></li> 
+								
+							</ul>
+			</div>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3074.7905052320443!2d-77.84987248482734!3d39.586871613613056!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c9f6a80ccf0661%3A0x7210426c67abc40!2sVirginia+Welcome+Center%2FSafety+Rest+Area!5e0!3m2!1sen!2sin!4v1485760915662" ></iframe>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</section>
+<?php include('includes/footer.php');?>
+</body>
+</html>
